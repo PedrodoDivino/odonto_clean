a91e7932008dd1508acb78aab23dc1f5376ad94f
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-18 21:14:19 -0300

    [Pedro] Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Primeiro projeto 
         </a>
       </header>
     </div>
