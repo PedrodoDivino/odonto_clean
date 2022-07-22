862069c1d14d63c02ca6c67084289e3703da2e0a
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-22 14:17:30 -0300

    [Pedro] ajustando margens, e retirando botão sobreando de tratamentos

--- src/components/pages/Treatments/index.tsx
+++ src/components/pages/Treatments/index.tsx
@@ -14,34 +14,23 @@ export const Treatments = () => {
   const [state, setState] = useState(4);
   return (
     <Box w="full" h="auto" mt="64px">
-      <Heading size="xl" my="3" mb="16px">
+      <Heading size="xl"  mb="-64px">
         Tratamentos
       </Heading>
 
       <SimpleGrid columns={[1, 2]}>
         {arrayTreatments.slice(0, state).map((infos, i) => {
           return (
-            <LinkBox key={infos.id}>
-            
+            <LinkBox mt="94px" mr="30px" key={infos.id}>
                 <Heading textAlign="left" display={"flex"} justifyContent="start">
-                  <Image src={infos.icon} />
-                  <LinkOverlay>{infos.title}</LinkOverlay>
+                  <Image src={infos.icon}mb="9px" marginRight="7px" />
+                  <LinkOverlay mb="9px">{infos.title} </LinkOverlay>
                 </Heading>
-             
-              <Text textAlign="left">{infos.text}</Text>
+              <Text textAlign="left" >{infos.text}</Text>
             </LinkBox>
           );
         })}
       </SimpleGrid>
-      {/* {state <= 4 ?  (< button onClick={()=>{
-        setState(6);
-
-     }}>Mostrar mais</button>):(
-        < button onClick={()=>{
-            setState(4);
-    
-         }}>Mostrar menos</button>
-     )}  */}
       {
         <button
           onClick={() => {
