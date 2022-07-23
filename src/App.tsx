885fdc4a9033cbc8fb740df5659d45534dc59bcd
gustavodev200 <gustavolage200@gmail.com>
2022-07-23 12:20:00 -0300

    [Gustavo] adicionado cor global de fundo e a fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { Home } from "./components/pages/Home";
 import WithSubnavigation from "./components/navbar/navbar";
 import { Differential } from "./components/pages/Differential";
 import { RetanguloPage } from "./components/pages/RetanguloPage";
+import { theme } from "./styles/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Container maxW={{ sm: "768px", lg: "960px", xl: "1200px" }} h="100vh">
         <WithSubnavigation />
         <Home />
