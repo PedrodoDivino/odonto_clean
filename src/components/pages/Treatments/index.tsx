e00ba159d591154e422cfc5159482ad9a7f0280d
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-25 20:41:05 -0300

    [Pedro] Ternário para classe

--- src/components/pages/Treatments/index.tsx
+++ src/components/pages/Treatments/index.tsx
@@ -8,32 +8,46 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { ButtonWrapper } from "../../ButtonWrapper/index"
-import  seta_baixo from "../../../assets/images/seta_baixo.svg"
+import { ButtonWrapper } from "../../ButtonWrapper/index";
+import seta_baixo from "../../../assets/images/seta_baixo.svg";
 
 import { arrayTreatments } from "../../../helpers/arrayTreatments";
 
 export const Treatments = () => {
   const [state, setState] = useState(4);
+
   return (
     <Box w="full" h="auto" mt="64px">
-      <Heading size="xl"  mb="-64px">
+      <Heading size="xl" mb="-64px">
         Tratamentos
       </Heading>
 
       <SimpleGrid columns={[1, 2]}>
+        {/* [] */}
         {arrayTreatments.slice(0, state).map((infos) => {
           return (
             <LinkBox mt="94px" mr="30px" key={infos.id}>
-                <Heading textAlign="left" display={"flex"} justifyContent="start">
-                  <Image src={infos.icon}mb="9px" marginRight="7px" />
-                  <LinkOverlay mb="9px">{infos.title} </LinkOverlay>
-                </Heading>
-             { state <=4 ? (
-               <Text textAlign="left" opacity={(state <=4 && infos.id == 3) || (infos.id == 4) ? "25%" : "none"} >{infos.text}</Text>
-             ):(
-              <Text textAlign="left" opacity= "none" >{infos.text}</Text>
-             )}
+              <Heading textAlign="left" display={"flex"} justifyContent="start">
+                <Image src={infos.icon} mb="9px" marginRight="7px" />
+                <LinkOverlay mb="9px">{infos.title} </LinkOverlay>
+              </Heading>
+              {state <= 4 ? (
+                <Text
+                  textAlign="left"
+                  className={infos.id === 3 || infos.id === 4 ? "text" : ""}
+                  opacity={
+                    (state <= 4 && infos.id === 3) || infos.id == 4
+                      ? "25%"
+                      : "none"
+                  }
+                >
+                  {infos.text}
+                </Text>
+              ) : (
+                <Text textAlign="left" opacity="none">
+                  {infos.text}
+                </Text>
+              )}
             </LinkBox>
           );
         })}
@@ -44,22 +58,24 @@ export const Treatments = () => {
             state <= 4 ? setState(6) : setState(4);
           }}
         >
-          {state <= 4 ? < ButtonWrapper 
-           bgColor="linear(rgba(32, 152, 227, 1), rgba(12, 99, 223, 1))"
-           imgIcon={seta_baixo}
-            text="Mostrar mais"
-            width={"178px"}
-            marginTop={"15px"}
-            padding={"20px"}
-            /> 
-           : 
-           < ButtonWrapper 
-            bgColor="linear(rgba(32, 152, 227, 1), rgba(12, 99, 223, 1))"
-            imgIcon={seta_baixo}
-            text="Mostrar Menos"
-            width={"178px"}
-            padding={"20px"}
-            />}
+          {state <= 4 ? (
+            <ButtonWrapper
+              bgColor="linear(rgba(32, 152, 227, 1), rgba(12, 99, 223, 1))"
+              imgIcon={seta_baixo}
+              text="Mostrar mais"
+              width={"178px"}
+              marginTop={"15px"}
+              padding={"20px"}
+            />
+          ) : (
+            <ButtonWrapper
+              bgColor="linear(rgba(32, 152, 227, 1), rgba(12, 99, 223, 1))"
+              imgIcon={seta_baixo}
+              text="Mostrar Menos"
+              width={"178px"}
+              padding={"20px"}
+            />
+          )}
         </button>
       }
     </Box>
