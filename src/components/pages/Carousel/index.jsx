e21e853c7673de0869e2e6dad31e5a4854fa9e13
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-28 18:51:41 -0300

    [Pedro] Adição de comnponente

---
+++ src/components/pages/Carousel/index.jsx
@@ -0,0 +1,53 @@
+import {
+    Box,
+    Heading,
+    Image,
+    LinkBox,
+    LinkOverlay,
+    SimpleGrid,
+    Text,
+  } from "@chakra-ui/react";
+  
+  import { arrayCarousel } from "../../../helpers/arrayCarousel.ts";
+  
+  export const Carousel = () => {
+    return (
+      <Box
+        w="full"
+        h="auto"
+        mt="64px"
+        borderBottom="2px solid rgba(2, 14, 31, 0.09)"
+      >
+        <Heading size="xl" my="3" mb="35px">
+          Dentistas Certificados
+        </Heading>
+  
+        <SimpleGrid columns={[1, 2, 4]}  mb="100px">
+          {arrayCarousel.map((infos) => (
+            <LinkBox
+        
+              as="article"
+              maxW="sm"
+              borderWidth="1px"
+              rounded="md"
+              key={infos.id}
+            >
+              <Box display="flex" justifyContent="center" >
+                <Image
+                  src={infos.image}
+                  bgColor="rgba(32, 152, 227, 0.15)"
+                  borderTopLeftRadius={30}
+                />
+              </Box>
+              <Heading size="md" my="2" mb="10px" textAlign="center">
+                <LinkOverlay href="#">{infos.title}</LinkOverlay>
+              </Heading>
+              <Text textAlign="center">{infos.text}</Text>
+              <Text textAlign="center">{infos.description}</Text>
+            </LinkBox>
+          ))}
+        </SimpleGrid>
+      </Box>
+    );
+  };
+  
--- src/helpers/arrayCarousel.ts
+++ src/helpers/arrayCarousel.ts
@@ -5,33 +5,33 @@ import  Foto_doutor3 from "../assets/images/Foto_doutor3.png";
 import  Foto_doutor4 from "../assets/images/Foto_doutor4.png";
 
 type JsonProps = {
-  icon: string;
+  image: string;
   title: string;
   description: string;
   id: number;
 };
 
-export const arrayTreatments: JsonProps[] = [
+export const arrayCarousel: JsonProps[] = [
   {
     id: 1,
-    icon: Foto_doutor1 ,
+    image: Foto_doutor1 ,
     title: "Dr. Pedro Ribeiro",
     description: "Ortodontista",
   },
   {
     id: 2,
-    icon: Foto_doutor2 ,
+    image: Foto_doutor2 ,
     title: "Dra. Katarina Alves",
     description: "Clínica geral",
   },
   {
     id: 3,
-    icon: Foto_doutor3 ,
+    image: Foto_doutor3 ,
     title: "Dr. Gustavo Ramos",
     description: "Odontopediatra",
   }, {
     id: 4,
-    icon: Foto_doutor4 ,
+    image: Foto_doutor4 ,
     title: "Dra. Melissa",
     description: "Implantodentista",
   },
