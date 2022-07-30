9ba27573e06654ee90b9d6247109cdfce9e52423
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-30 18:09:19 -0300

    [Pedro] Alterando margin e colocando fit-content

--- src/components/pages/Carousel/index.jsx
+++ src/components/pages/Carousel/index.jsx
@@ -28,7 +28,7 @@ if (!data || data.length) return null
   return (
     <Box
       w="full"
-      h="auto"
+      h="100%"
       mt="64px"
       borderBottom="2px solid rgba(2, 14, 31, 0.09)"
        >
@@ -39,27 +39,23 @@ if (!data || data.length) return null
       display= 'flex'
       overflowX='scroll'
       flexDirection='row'
-      >
+       alignItems='center'
+       >
         {arrayCarousel.map((infos) => (
-          <LinkBox
-
-            mr='15px'
-            minW="sm"
+          <LinkBox 
+          mr='10'
+            minW="fit-content"
             mb='20px'
-            borderWidth="1px"
             key={infos.id}
           >
-            <Box display="flex" 
-            justifyContent='center'
-             flexDirection='column'
-
-             >
               <Image
                 src={infos.image}
                 bgColor="rgba(32, 152, 227, 0.15)"
                 borderTopLeftRadius={30}
+                borderWidth="10px"
+
+
               />
-            </Box>
             <Heading size="md" my="2" mb="10px" textAlign="center">
               <LinkOverlay href="#">{infos.title}</LinkOverlay>
             </Heading>
